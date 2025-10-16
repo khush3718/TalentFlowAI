7f39c2ff23c0a064cf64a7995a7774a41ecc24b2
Khush Shah <74204845+khush3718@users.noreply.github.com>
2025-10-16 17:29:20 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -375,5 +375,6 @@ async def shutdown_event():
 # ==================== MAIN ====================
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", reload=True)
+
 
